d196358080cca1c742ed4627730ecdbcbf0f687b
ko-math <sorehakiemasu@gmail.com>
2026-09-18 17:15:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const $d = document;
 //lastModified
 (() => {
   const content = $d.querySelector('#lastModified');
+  const lastModified = new Date($d.lastModified);
+  content.textContent = `
+    lastModified: ${lastModified.getFullYear()}-${lastModified.getMonth() + 1}-${lastModified.getDate()}
+  `;
 })();
